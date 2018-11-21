ab7256244c4cfe60282dab4c0be6245a4c7c4853
scatterbrainz <mike.deslandes@gmail.com>
2018-11-21 13:54:14 -0500

    test commit for group to pull

---
+++ instock-server/LocationData.js
@@ -0,0 +1,71 @@
+module.exports = {
+
+    warehouselocations:[
+        {
+            "id": "001", 
+            "title": "Toronto",
+            "locationCode": "YYZ", 
+            "address":{
+                "streetAddress": "123 Renforth Drive",
+                "city": "Toronto", 
+                "postalcode": "M5J2H2", 
+            },
+            "contact": {
+                "name": "Mara Weinburg", 
+                "title": "Warehouse Manager",
+                "phonenumber": "14166782345",
+                "email": "mweinberg@instock.com"
+            },
+        },
+        {
+            "id": "002", 
+            "title": "Vancouver",
+            "locationCode": "YVR", 
+            "address":{
+                "streetAddress": "4500 North Service Road",
+                "city": "Vancouver", 
+                "provice": "British Columbia", 
+                "postalcode": "V3Y2A8", 
+            },
+            "contact": {
+                "name": "Mara Weinburg", 
+                "title": "Warehouse Manager",
+                "phonenumber": "16046782345",
+                "email": "mweinberg@instock.com"
+            }
+        },
+        {
+            "id": "003", 
+            "title": "Regina",
+            "locationCode": "YQR", 
+            "address":{
+                "streetAddress": "300 Airport Road",
+                "city": "Regina", 
+                "province": "Saskatchewan",
+                "postalcode": "V3Y2A8", 
+            },
+            "contact": {
+                "name": "Mara Weinburg", 
+                "title": "Warehouse Manager",
+                "phonenumber": "13066782345",
+                "email": "mweinberg@instock.com"
+            }
+        },
+        {
+            "id": "004", 
+            "title": "Montreal",
+            "locationCode": "YUL", 
+            "address":{
+                "streetAddress": "50 Sources Boulevard",
+                "city": "Quebec", 
+                "postalcode": "H9A5T1", 
+            },
+            "contact": {
+                "name": "Mara Weinburg", 
+                "title": "Warehouse Manager",
+                "phonenumber": "15146782345",
+                "email": "mweinberg@instock.com"
+            }
+        }
+    ]
+}
