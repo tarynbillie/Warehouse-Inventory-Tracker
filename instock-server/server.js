bbf98174c54d009e27835552ad0c65c0c68611e5
Taryn Li <tarynli@hotmail.com>
2018-11-21 11:32:50 -0500

    Instock app created, along with server side

---
+++ instock-server/server.js
@@ -0,0 +1,6 @@
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE');
+    next();
+});
