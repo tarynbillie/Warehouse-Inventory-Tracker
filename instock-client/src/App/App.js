02a658e096e525ec64378d2978f7de5e7fd4e6d4
Ryan Stairs <ryanstairs@gmail.com>
2018-11-22 22:56:01 -0500

    Browser

--- instock-client/src/App/App.js
+++ instock-client/src/App/App.js
@@ -1,38 +1,15 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-// import logo from './logo.svg';
-import './App.css';
-import Main from '../Main/Main'
-=======
-import {BroswerRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
 import Warehouse from './Warehouse.js';
-import Inventory from './Inventory.js';
+import Inventory from '../Main/Inventory/Inventory.js';
 import WarehouseDetails from './WarehouseDetails.js';
 import InventoryItems from './InventoryItems.js';
->>>>>>> 84ddafc067f9cacddd3c328742cfbbcaa77a17eb
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
-        <Main />
-=======
       <Router>
         <Switch>
           <Route path={'/warehouses'} exact component={Warehouse}/>
@@ -42,7 +19,6 @@ class App extends Component {
           <Route path={'/'} render={() => <Redirect to='/warehouses' />} />
         </Switch>
       </Router>
->>>>>>> 84ddafc067f9cacddd3c328742cfbbcaa77a17eb
       </div>
     );
   }
