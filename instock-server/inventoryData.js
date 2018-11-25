37899b93baed175a6c17967bfea811309af9feca
Cameron Pentland <cameronpentland@gmail.com>
2018-11-25 15:34:40 -0500

    added get inventory endpoint

--- instock-server/inventoryData.js
+++ instock-server/inventoryData.js
@@ -1,36 +1,3 @@
-const express = require('express');
-const app = express();
-//you will need to create 'productInventoryList' for below line to work 
-// const productList = require('productInventoryList');
-const productList = [];
-// delete above empty array when file is created and available. 
-
-const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 8080;
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-
-app.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE');
-    next();
-});
-
-xprex.get('/product/:id', (req, res) => {
-    const productCode = parseInt(req.params.id);
-    const productItem = productList.find((pItem) =>
-    { return pItem.id === productCode})
-    console.log(productItem);
-    res.json(productItem); 
-  })
-
-
-
-
-//--CAM- get all inventory--//
 inventory= [
     {
         productID: 1,
@@ -137,20 +104,3 @@ inventory= [
         status: 'in stock'
     },
 ]
-
-
-
-app.get('/inventory', (req,res)=>{
-    res.json(inventory)
-})
-
-
-
-
-
-app.listen(PORT, (err) => {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(`server is listening on ${PORT}`)
-})
